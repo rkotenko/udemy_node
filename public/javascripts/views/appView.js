530991fb4e33d77cedbb180be2808c7d7295b7c5
rkotenko <robkotenko@gmail.com>
2015-02-17 13:11:48 -0500

    Lesson 14 completed - new posts enabled
    Added a form that allows users to submit new posts and have them save
    to server and appear in the post list.

--- public/javascripts/views/appView.js
+++ public/javascripts/views/appView.js
@@ -2,11 +2,25 @@ var app = app || {};
 
 app.AppView = Backbone.View.extend({
     el: '#blog',
+    events: {
+        'submit #newpost': 'newPostSubmit'
+    },
     initialize: function () {
         this.listenTo(app.posts, 'add', this.addOne);
     },
     addOne: function (post) {
         var view = new app.PostView({model: post});
         this.$('#posts').append(view.render().el);
+    },
+    newPostSubmit: function (e) {   
+        e.preventDefault();
+        var attributes = {
+            title: this.$('[name="title"]').val(),
+            content: this.$('[name="content"]').val(),
+            posted: new Date()
+        };
+        
+        app.posts.create(attributes);
+        this.$('[type="text"]').val('');
     }
 });
